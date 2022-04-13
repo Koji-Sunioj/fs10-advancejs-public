ba8aed4c95a1e6fc7f07ac74f5adafd7227c94ec
Koji Inoue <koji.gabriel218@gmail.com>
2022-04-13 11:34:31 +0300

    done

--- assignment-2/index.js
+++ assignment-2/index.js
@@ -1,36 +1,75 @@
 
+handleError = (response)=>{
+    if (response.ok){
+        return response.json()
+    }
+    else{
+        throw new Error('no resource found')
+    }
+}
+
 
 class Country {
-    constructor(name,countries)
-    {
-        this.name = name
-        this.countries = countries
+    
+    constructor(country,iso){
+        this.country = country
+        this.iso = iso
     }
     
-    getCountries(){
-        
-        const countryPromise = fetch('https://restcountries.com/v3.1/all').then((response)=>
-        response.json()).then(data=>data)
-        return countryPromise
+    getCountries(population=false){
+        const countries = fetch('https://restcountries.com/v3.1/all').then((response)=>
+            handleError(response)
+            ).then(data=>{
+            if (population)
+            {
+                data = data.filter(country => country.population> population)
+            }
+            return data
+            }).catch(()=>'nothing found')
+        return countries
     }
 
 
-    getCountry(name){  
-        const country = fetch(`https://restcountries.com/v2/name/${name}`).then((response)=>
-        response.json()).then(data=>data)
+    getCountryInfo(borders=false){  
+        const country = fetch(`https://restcountries.com/v2/name/${this.country}`).then((response)=>
+            handleError(response)
+            ).then(data=>{
+            if (data.length == 1 && borders == true)
+            {
+                data = data[0].borders
+            }
+            return data
+            }).catch(()=>'nothing found')
         return country
+    }
+
+    getLangCountries(){  
+        const countries = fetch(`https://restcountries.com/v2/lang/${this.iso}`).then((response)=>
+            handleError(response)
+        ).then(data=>{return data}).catch(()=>'nothing found')
+        return countries
     }
 
 }
 
 async function init(){
-    const Finland = new Country('Finland')
-    console.log(Finland.name)
-    const countries =  await Finland.getCountries()
+
+    //1. method to get all countries
+    const countries = await Country.prototype.getCountries()
     console.log(countries)
-    const country = await Finland.getCountry('Finland')
-    console.log(country)
+
+    const Finland = new Country('Suomi','fi')
+    //2. A method that accept a country name as input and search for that country 
+    console.log(await Finland.getCountryInfo())
+    //3. get border names
+    console.log(await Finland.getCountryInfo(borders=true))
+    //4. get language of countries
+    console.log(await Finland.getLangCountries())
+
+    //5. accepts a population number (in millions),  what countries have more people than that
+    const pops = await Country.prototype.getCountries(population=5000000)
+    console.log(pops)
 }
 
+init()
 
-document.addEventListener('DOMContentLoaded',init)
