271b047eb67652508b2d0d0cccc2c7be4a2a5d1c
Koji Inoue <koji.gabriel218@gmail.com>
2022-04-13 16:44:41 +0300

    added 2 space indentation hope this is what you meant thanks

--- assignment-2/index.js
+++ assignment-2/index.js
@@ -1,74 +1,71 @@
 
 handleError = (response)=>{
-    if (response.ok){
-        return response.json()
-    }
-    else{
-        throw new Error('no resource found')
-    }
+  if (response.ok){
+    return response.json()
+  }
+  else{
+    throw new Error('no resource found')
+  }
 }
 
-
 class Country {
+  constructor(country,iso){
+    this.country = country
+    this.iso = iso
+  }
     
-    constructor(country,iso){
-        this.country = country
-        this.iso = iso
-    }
+  getCountries(population=false){
+    const countries = fetch('https://restcountries.com/v3.1/all').then((response)=>
+      handleError(response)
+      ).then(data=>{
+      if (population)
+      {
+        data = data.filter(country => country.population> population)
+      }
+      return data
+      }).catch(()=>'nothing found')
+      return countries
+  }
+
+  getCountryInfo(borders=false){  
+    const country = fetch(`https://restcountries.com/v2/name/${this.country}`).then((response)=>
+      handleError(response)
+      ).then(data=>{
+      if (data.length == 1 && borders == true)
+      {
+          data = data[0].borders
+      }
+      return data
+      }).catch(()=>'nothing found')
+      return country
+  }
     
-    getCountries(population=false){
-        const countries = fetch('https://restcountries.com/v3.1/all').then((response)=>
-            handleError(response)
-            ).then(data=>{
-            if (population)
-            {
-                data = data.filter(country => country.population> population)
-            }
-            return data
-            }).catch(()=>'nothing found')
-        return countries
-    }
-
-
-    getCountryInfo(borders=false){  
-        const country = fetch(`https://restcountries.com/v2/name/${this.country}`).then((response)=>
-            handleError(response)
-            ).then(data=>{
-            if (data.length == 1 && borders == true)
-            {
-                data = data[0].borders
-            }
-            return data
-            }).catch(()=>'nothing found')
-        return country
-    }
-
-    getLangCountries(){  
-        const countries = fetch(`https://restcountries.com/v2/lang/${this.iso}`).then((response)=>
-            handleError(response)
-        ).then(data=>{return data}).catch(()=>'nothing found')
-        return countries
-    }
+  getLangCountries(){  
+    const countries = fetch(`https://restcountries.com/v2/lang/${this.iso}`).then((response)=>
+        handleError(response)
+    ).then(data=>{return data}).catch(()=>'nothing found')
+    return countries
+  }
 
 }
 
 async function init(){
 
-    //1. method to get all countries
-    const countries = await Country.prototype.getCountries()
-    console.log(countries)
+  //1. method to get all countries
+  const countries = await Country.prototype.getCountries()
+  console.log(countries)
 
-    const Finland = new Country('Suomi','fi')
-    //2. A method that accept a country name as input and search for that country 
-    console.log(await Finland.getCountryInfo())
-    //3. get border names
-    console.log(await Finland.getCountryInfo(borders=true))
-    //4. get language of countries
-    console.log(await Finland.getLangCountries())
+  const Finland = new Country('Suomi','fi')
+  //2. A method that accept a country name as input and search for that country 
+  console.log(await Finland.getCountryInfo())
+  //3. get border names
+  console.log(await Finland.getCountryInfo(borders=true))
+  //4. get language of countries
+  console.log(await Finland.getLangCountries())
 
-    //5. accepts a population number (in millions),  what countries have more people than that
-    const pops = await Country.prototype.getCountries(population=5000000)
-    console.log(pops)
+  //5. accepts a population number (in millions),  what countries have more people than that
+  const pops = await Country.prototype.getCountries(population=5000000)
+  console.log(pops)
 }
 
 init()
